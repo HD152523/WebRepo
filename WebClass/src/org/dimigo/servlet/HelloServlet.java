510b88c3f7f9975c3d66c9e79f26ca626d13b96e
HD152523 <junho7108@naver.com>
2017-09-25 14:37:04 +0900

    Add files via upload

---
+++ WebClass/src/org/dimigo/servlet/HelloServlet.java
@@ -0,0 +1,57 @@
+package org.dimigo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+
+@WebServlet("/Hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		String name = request.getParameter("name");
+		
+		response.setContentType("text/html:charset = utf8");
+		PrintWriter out = response.getWriter();
+		
+	
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");out.println("<head>");
+		out.println("<title>Servlet Test</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>저는"+ name+ "입니다.</h1>");
+		out.println("</body></html>");out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
